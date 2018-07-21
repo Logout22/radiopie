4450cb12b295c1b4eb5435e451d423e3b9ebf258
Martin Unzner <martin.u@posteo.de>
2018-07-21 21:13:22 +0200

    add control

---
+++ control.py
@@ -0,0 +1,18 @@
+#!  /usr/bin/env python3
+""" Control web radio and screen operation """
+import unittest
+
+class TestControl(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_show_welcome(self):
+        pass
+
+class Control(object):
+    """ Control the radio unit """
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
