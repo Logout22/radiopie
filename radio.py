cd045956382eba35980249f40513e8ce504c9b9b
Martin Unzner <martin.u@posteo.de>
2018-07-21 14:02:39 +0200

    make radio resilient against section reordering

--- radio.py
+++ radio.py
@@ -1,6 +1,7 @@
 #!  /usr/bin/env python3
 """ Navigate through radio stations and trigger playback """
 
+from random import randint
 import unittest
 
 class TestRadio(unittest.TestCase):
@@ -16,68 +17,74 @@ class TestRadio(unittest.TestCase):
             return self._last_url
 
     def setUp(self):
+        self.general = {'name': 'general', 'content': {'last_station': 1}}
         self.stations = [
-            {'name': 'general', 'content': {'last_station': 2}},
             {'name': 'station1', 'content': {'url': 'http://example.com'}},
             {'name': 'station2', 'content': {'url': 'http://example.org'}},
             {'name': 'station3', 'content': {'url': 'http://example.net'}}
             ]
         self.player = self.FakePlayer()
-        self.radio = Radio(self.stations, self.player)
+        config = self.stations[:]
+        config.insert(randint(0, 3), self.general)
+        self.radio = Radio(config, self.player)
 
     def test_play_last_station(self):
         self.assertEqual('station2', self.radio.get_playing_station()['name'])
         self.assertEqual('http://example.org', self.player.get_last_url())
 
     def test_play_next_station(self):
-        new_stations = self.radio.play_next_station()
-        self.assertEqual('station3', self.radio.get_playing_station()['name'])
-        self.assertEqual(new_stations[0]['content']['last_station'], 3)
-        self.assertEqual('http://example.net', self.player.get_last_url())
+        expected_new_index = 2
+        self.radio.play_next_station()
+        self._verify_station_switched(expected_new_index)
 
     def test_play_previous_station(self):
-        new_stations = self.radio.play_previous_station()
-        self.assertEqual('station1', self.radio.get_playing_station()['name'])
-        self.assertEqual(new_stations[0]['content']['last_station'], 1)
-        self.assertEqual('http://example.com', self.player.get_last_url())
+        expected_new_index = 0
+        self.radio.play_previous_station()
+        self._verify_station_switched(expected_new_index)
+
+    def _verify_station_switched(self, expected_new_index):
+        expected_station = self.stations[expected_new_index]
+        self.assertEqual(expected_station['name'], self.radio.get_playing_station()['name'])
+        self.assertEqual(expected_new_index, self.general['content']['last_station'])
+        self.assertEqual(expected_station['content']['url'], self.player.get_last_url())
 
 class Radio(object):
     """ Control the radio unit """
-    def __init__(self, stations, media_player):
-        self._stations = stations
-        self._playing_station = self._stations[0]['content']['last_station']
+    def __init__(self, config, media_player):
+        self._stations = []
+        self._load_config(config)
         self._media_player = media_player
         self._invoke_media_player()
 
     def get_playing_station(self):
         """ Return an object describing the station that currently plays. """
-        return self._stations[self._playing_station]
+        return self._stations[self._general['last_station']]
 
     def play_next_station(self):
         """ Play the next station in the array of stations given at construction. """
         self._select_next_station()
         self._invoke_media_player()
-        self._update_last_station()
-        return self._stations
 
     def play_previous_station(self):
         """ Play the next station in the array of stations given at construction. """
         self._select_previous_station()
         self._invoke_media_player()
-        self._update_last_station()
-        return self._stations
+
+    def _load_config(self, config):
+        for section in config:
+            if section['name'] == 'general':
+                self._general = section['content']
+            else:
+                self._stations.append(section)
 
     def _select_next_station(self):
-        self._playing_station = (self._playing_station + 1) % len(self._stations)
+        self._general['last_station'] = (self._general['last_station'] + 1) % len(self._stations)
 
     def _select_previous_station(self):
-        self._playing_station = (self._playing_station - 1) % len(self._stations)
-
-    def _update_last_station(self):
-        self._stations[0]['content']['last_station'] = self._playing_station
+        self._general['last_station'] = (self._general['last_station'] - 1) % len(self._stations)
 
     def _invoke_media_player(self):
-        self._media_player.play_url(self._stations[self._playing_station]['content']['url'])
+        self._media_player.play_url(self.get_playing_station()['content']['url'])
 
 if __name__ == '__main__':
     unittest.main()
