f73532d1218c7749aa6c04911fb1a086e1feaa09
Martin Unzner <martin.u@posteo.de>
2018-07-21 12:26:54 +0200

    add interface for media playback

--- radio.py
+++ radio.py
@@ -4,6 +4,17 @@
 import unittest
 
 class TestRadio(unittest.TestCase):
+    class FakePlayer(object):
+        """ Fake media player class which captures function call from Radio """
+        def __init__(self):
+            self._last_url = ""
+
+        def play_url(self, url):
+            self._last_url = url
+
+        def get_last_url(self):
+            return self._last_url
+
     def setUp(self):
         self.stations = [
             {'name': 'general', 'content': {'last_station': 2}},
@@ -11,26 +22,32 @@ class TestRadio(unittest.TestCase):
             {'name': 'station2', 'content': {'url': 'http://example.org'}},
             {'name': 'station3', 'content': {'url': 'http://example.net'}}
             ]
-        self.radio = Radio(self.stations)
+        self.player = self.FakePlayer()
+        self.radio = Radio(self.stations, self.player)
 
     def test_play_last_station(self):
         self.assertEqual('station2', self.radio.get_playing_station()['name'])
+        self.assertEqual('http://example.org', self.player.get_last_url())
 
     def test_play_next_station(self):
         new_stations = self.radio.play_next_station()
         self.assertEqual('station3', self.radio.get_playing_station()['name'])
         self.assertEqual(new_stations[0]['content']['last_station'], 3)
+        self.assertEqual('http://example.net', self.player.get_last_url())
 
     def test_play_previous_station(self):
         new_stations = self.radio.play_previous_station()
         self.assertEqual('station1', self.radio.get_playing_station()['name'])
         self.assertEqual(new_stations[0]['content']['last_station'], 1)
+        self.assertEqual('http://example.com', self.player.get_last_url())
 
 class Radio(object):
     """ Control the radio unit """
-    def __init__(self, stations):
+    def __init__(self, stations, media_player):
         self._stations = stations
         self._playing_station = self._stations[0]['content']['last_station']
+        self._media_player = media_player
+        self._invoke_media_player()
 
     def get_playing_station(self):
         """ Return an object describing the station that currently plays. """
@@ -38,18 +55,29 @@ class Radio(object):
 
     def play_next_station(self):
         """ Play the next station in the array of stations given at construction. """
-        self._playing_station = (self._playing_station + 1) % len(self._stations)
+        self._select_next_station()
+        self._invoke_media_player()
         self._update_last_station()
         return self._stations
 
     def play_previous_station(self):
         """ Play the next station in the array of stations given at construction. """
-        self._playing_station = (self._playing_station - 1) % len(self._stations)
+        self._select_previous_station()
+        self._invoke_media_player()
         self._update_last_station()
         return self._stations
 
+    def _select_next_station(self):
+        self._playing_station = (self._playing_station + 1) % len(self._stations)
+
+    def _select_previous_station(self):
+        self._playing_station = (self._playing_station - 1) % len(self._stations)
+
     def _update_last_station(self):
         self._stations[0]['content']['last_station'] = self._playing_station
+
+    def _invoke_media_player(self):
+        self._media_player.play_url(self._stations[self._playing_station]['content']['url'])
 
 if __name__ == '__main__':
     unittest.main()
